32e972c1b06f4b0bd992e80a7eee3099ddaf243b
afzsilva <afzsilva@gmail.com>
2023-09-20 18:11:19 -0300

    cria componente welcome e adiciona background image

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { MenuNavBarComponent } from './components/menu-nav-bar/menu-nav-bar.component';
+import { WelcomeSectionComponent } from './components/welcome-section/welcome-section.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    MenuNavBarComponent
+    MenuNavBarComponent,
+    WelcomeSectionComponent,
+
   ],
   imports: [
     BrowserModule,
